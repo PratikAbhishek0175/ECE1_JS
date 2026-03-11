9aa1bc7093f6260358671693cec967c6dffea7a5
Pratik Abhishek <pratikabhishek1810@gmail.com>
2026-03-11 10:30:31 +0530

    commit

---
+++ function.js
@@ -0,0 +1,29 @@
+//Function dowhatever() {statement}
+//function myfunction()
+//{
+//    alert("Hello World");
+//}
+
+
+//Arrow function is Anonymous and change the way this binds in functions.
+//let add = (a,b) =>
+//{
+//    document.write("The result of addition is:" + (a + b));
+//};
+
+
+let person = {
+    name:"Ajay",
+    age: 25,
+    isMarried: true,
+    address:{
+        street:'vinay nagar'
+        city:'Delhi'
+    }
+};
+document.write(person["name"] + )
+
+
+//difference between an object and an array
+//Array is an ordered collection of data but object is an unordered collection of data
+//array -- square brackets and object -- curly brackets
--- script.js
+++ script.js
@@ -27,3 +27,15 @@ else
 {
     console.log("false")
 }
+for (key in object)
+{
+
+}
+const person = {
+    fname:"Ajay",
+    lname:"Singh",
+    age:25
+};
+for (let x in person){
+    console.log("person details:" + x + ":" + person[x])
+}
