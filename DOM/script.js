2e0eb1c14f9b6622b737435fd57dae69ec63c256
Pratik Abhishek <pratikabhishek1810@gmail.com>
2026-03-18 09:57:25 +0530

    DOM commit

---
+++ DOM/script.js
@@ -0,0 +1,26 @@
+//DOM in JS
+//javascript can change all the html elements in the page
+//javascript can change all the html attributes in the page
+//javascript can change all the css style in the page
+//javascript can remove existing html elements and attributes
+//javascript can add new html elements and attributes
+//javascript can react to all existing html events in the page
+//javascript can create new html events in the page
+//for css property we use element.style.property 
+//for creating element document.createElement("h2");
+//for getting parent document.getElementsByTagName("body");
+//add elenemt at the position using append method bodytag[0].append(heading);
+//node.prepend() //adds at the start of node(inside)
+//node.before() //adds before the node(outside)
+//node.after() //adds after the node(outside)
+
+
+//const result=document.getElementById("jk");
+//console.log(result);
+//result.innerHTML="<b>Hello World</b>"
+//const result=document.getElenentById("p1");
+//console.log(result);
+//result1[1].innerHTML="chauhan";
+//result1[0].innerHTML="Jitendra";
+//const result=document.getElementById("jk").getAttribute("class")
+//document.getElementById("p1").innerHTML=result;
